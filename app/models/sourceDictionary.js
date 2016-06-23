4c41849b30c13fa94de623f9475b2bf3484fb8a0
yan <yliang@ou.edu>
2016-06-22 22:15:54 -0500

    fix the sentence button not working on showMOstRecentDialog and also delete jqueryTab,js to fix a console error

--- app/models/sourceDictionary.js
+++ app/models/sourceDictionary.js
@@ -2,20 +2,6 @@ var mongoose=require("mongoose");
 var FormatDate=mongoose.Schema.Types.FormatDate=require('../../node_modules/mongoose-schema-formatdate/formatdate');
 var mongoosePaginate = require('mongoose-paginate');
 
-/*module.exports=mongoose.model('sourceDictionary',{
- sentenceId:{type:String},
- word:{type:String},
- countryCode:{type:String},
- firstRoleCode:{type:String},
- secondRoleCode:{type:String},
- dateStart:{type:FormatDate,format:'YYYY-MM-DD'},
- dateEnd:{type:FormatDate,format:'YYYY-MM-DD'},
- confidenceFlag:{type:Boolean},
- userId:{type:String},
- userName:{type:String},
- taggingTime:{type:Date,default:Date.now}
-});*/
-
 var schema = new mongoose.Schema({
  sentenceId:{type:String},
  word:{type:String},
