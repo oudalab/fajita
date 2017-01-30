f0ce5a854dd206262bd423346d3698a87b811b7d
yan <yliang@ou.edu>
2017-01-29 23:18:12 -0600

    add tagging time for verb and also add the config file with mongo crendetial to git ignore #113 #114

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
 
-	url: 'mongodb://localhost:27017/eventData'
+	//url: 'mongodb://localhost:27017/eventData'
 		//url:'mongodb://dalab.ou.edu:27017/lexisnexis'
+         url:'mongodb://boomer:burritos_for_breakfast@portland.cs.ou.edu:23755/lexisnexis'
 }
