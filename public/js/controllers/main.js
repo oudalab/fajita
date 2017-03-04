56763387eecfaa66f8f0a96265adac691f8fc023
yan <yliang@ou.edu>
2017-03-04 12:48:23 -0600

    #144

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -148,6 +148,9 @@ app.controller('mainController', ['$scope', '$http', '$location', 'Actors', 'Age
           $('.custom-combobox:eq(0) .custom-combobox-input').val(data.countryCode);
           $('.custom-combobox:eq(1) .custom-combobox-input').val(data.firstRoleCode);
           $('.custom-combobox:eq(2) .custom-combobox-input').val(data.secondRoleCode);
+          $('#combobox0input').val(data.countryCode);
+          $('#combobox1input').val(data.firstRoleCode);
+          $('#combobox2input').val(data.secondRoleCode);
                                
         } else {
             //do nothing since the form has been all cleared.
@@ -310,7 +313,6 @@ app.controller('mainController', ['$scope', '$http', '$location', 'Actors', 'Age
     var sourceDicObject = {
       word: word,
       sentenceId: sentenceId,
-      //this is they way to go there is bug in combobox auto complete use jquery directly
       countryCode: countryCode, //countryCode
       firstRoleCode: firstRoleCode, //firstRoleCode
       secondRoleCode: secondRoleCode, //secondRoleCode
@@ -321,6 +323,7 @@ app.controller('mainController', ['$scope', '$http', '$location', 'Actors', 'Age
       userid:localStorage.getItem('currentid')
         //get the useId from the req in api.
     };
+    //console.log("testtest: "+sourceDicObject);
     //if it is source push into sourcelist 
     var chosenOption = $("input:radio[name='inlineRadioOptions']:checked").val();
     if (chosenOption != "Other") {
