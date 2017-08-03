62196a3accf7e7140586fcc6ac8f6084e4efc847
Yan Liang <yanliang@Yans-MacBook-Pro.local>
2017-08-02 22:45:27 -0500

    #182

--- otherHelperCode/fastCodingEntity/gather_arabic_entity.py
+++ otherHelperCode/fastCodingEntity/gather_arabic_entity.py
@@ -1,7 +1,6 @@
-
 from pymongo import MongoClient
-import pickle
 import time
+import pickle
 client=MongoClient()
 client=MongoClient('mongodb://hanover.cs.ou.edu:29017/')
 db=client['eventData']
@@ -10,29 +9,25 @@ sen=db.sentence_arabic
 start_time=time.time()
 dic={}
 count=0
+totalEntity=0
 for i in sen.find():
     count=count+1;
     if(count%1000==0):
         print(str(count),"finished processing")
-    #if(count<limit):
-        id=i["_id"]
-        actors=i["actor"]
-        for actor in actors:
-            if not actor in dic:
-                dic[actor]=[]
-            dic[actor].append(id)
-    #else:
-        #break
-print(str(count),"total processed")
+    id=i["_id"]
+    actors=i["actor"]
+    for actor in actors:
+        totalEntity=totalEntity+1
+        if not actor in dic:
+            dic[actor]=[]
+        dic[actor].append(id)
+print(str(count),"total processed sentences")
 
 try:
     with open("arabicEntity.data",'wb') as f:
         pickle.dump(dic,f,pickle.HIGHEST_PROTOCOL)
 except Exception as e:
-    print(e)
-    pass
+        print(e)
 timepassed=time.time()-start_time
 print("time used in seconds ",str(timepassed))
-
-# with open("test1","rb") as f:
-#     hey=pickle.load(f)
+print("total entity found ",str(totalEntity))
