818969cfd91767e5d39c2283fedb094d45644d2d
yan <yliang@ou.edu>
2017-12-20 18:49:46 -0600

    wiki entity #203

---
+++ app/models/wikientity.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+
+module.exports = mongoose.model('wikientity', {
+
+	wiki_pages:[], //this will store an array of json objects including the name and wiki link, name in different language
+    wiki_roles:[], //which each element has the property of :title, start date, end date,
+    cameo_coding:[] //this will be a list of cameo coding result.
+});
